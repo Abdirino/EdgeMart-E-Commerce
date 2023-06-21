a029ec86abb844a3beb6e085d99ba6736299b7fb
Abdirino <abdirinoahash@gmail.com>
2023-06-21 10:30:11 +0300

    to be continued soon ✌️☕

---
+++ src/Components/HomeSlider/HSlider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HSlider.css'
+
+const HSlider = () => {
+  return (
+    <>
+        <h1>Slider</h1>
+    </>
+  )
+}
+
+export default HSlider
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Footer from '../../Components/Footer/Footer'
 import Offers from '../../Components/Offers/Offers'
 import Navbar from '../../Components/Navbar/Navbar'
-import HomeSlider from '../../Components/Slider/HomeSlider'
+import HSlider from '../../Components/HomeSlider/HSlider'
 
 const Home = () => {
   return (
     <>
         <Navbar />
-        <HomeSlider />
+        <HSlider />
         <Offers />
         <Footer />
     </>
