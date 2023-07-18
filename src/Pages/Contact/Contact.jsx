8bf77dadffe3a6dfba29226b751fcb3f9e7d4539
Abdirino <abdirinoahash@gmail.com>
2023-07-18 10:36:22 +0300

    contact sec start

---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './contact.css'
+import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer'
+
+const Contact = () => {
+  return (
+    <>
+        <Navbar />
+        <div className="contact"></div>
+        <Footer />
+    </>
+  )
+}
+
+export default Contact
